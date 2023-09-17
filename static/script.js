96b3fb91d2f7692cb37d31ee49f10394605713c2
Bryan Yoo <bryanyoo5433@gmail.com>
2023-09-17 08:19:11 -0400

    deleted javascript for animation stuff

--- static/script.js
+++ static/script.js
@@ -1,32 +1,3 @@
-//below is the animation front end
-let intro = document.querySelector('.intro');
-let logo = document.querySelector('.logo-header');
-let logoSpan = document.querySelectorAll('.logo');
-
-window.addEventListener('DOMContentLoaded', ()=>{
-  setTimeout(()=>{
-    logoSpan.forEach((span, idx)=>{
-      setTimeout(()=>{
-        span.classList.add('active');
-      }, (idx + 1) * 400)
-    });
-
-    setTimeout(()=>{
-      logoSpan.forEach((span, idx)=>{
-        setTimeout(()=>{
-          span.classList.remove('active');
-          span.classList.add('fade');
-        }, (idx + 1) * 50)
-      })
-    }, 2000);
-
-    setTimeout(()=>{
-      intro.style.top = '-100vh';
-    }, 2300)
-    
-  })
-});
-
 //below is the webcam stuff
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
